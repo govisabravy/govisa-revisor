b94184f350002b771b0d9aa3d831b3441cda0132
tiago1002bravy <tiago@asv.digital>
2026-04-21 22:49:20 -0300

    fix: explicit webpack alias for @/ to work on Linux

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse", "better-sqlite3"]
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   }
 };
 
